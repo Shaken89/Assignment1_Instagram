5df1b755485d26113d17fa9e3078af2a3ac99a6a
Shaken05 <122993715+Shaken05@users.noreply.github.com>
2025-09-20 17:14:32 +0500

    Instagram Login Screen UI

--- app/src/main/java/com/example/assignment1/InstagramLoginScreen.kt
+++ app/src/main/java/com/example/assignment1/InstagramLoginScreen.kt
@@ -2,6 +2,7 @@ package com.example.assignment1
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
